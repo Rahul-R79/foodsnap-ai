edbcf5c8ed59a9e86f415a924f396fe1a5c4d6f0
Rahul-R79 <rahul.devworks@gmail.com>
2026-01-04 15:11:34 +0530

    chore: added route to index file

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
+import analyzeRoute from "./routes/analyzeRoute";
 
 dotenv.config();
 connectDB();
@@ -11,10 +12,11 @@ const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
-
 app.get("/", (_, res) => {
     res.send("Hello from FoodSnap AI Backend!");
 });
+
+app.use("/api/v1", analyzeRoute);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
